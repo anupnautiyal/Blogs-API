a9b6fc5abd60612f13c440208697896f76448329
Anup Nautiyal <anupn8083@gmail.com>
2026-01-13 17:01:32 +0530

    New Update DELETE and PATCH

--- index.js
+++ index.js
@@ -73,8 +73,30 @@ app.post("/posts", (req, res) => {
 });
 
 //CHALLENGE 4: PATCH a post when you just want to update one parameter
+app.patch("/posts/:id", (req, res) => {
+  const postId = parseInt(req.params.id);
+  const post = posts.find((p) => p.id === postId);
+  if (!post) {
+    return res.status(404).json({ message: "Post not found" });
+  }
+  if (req.body.title)post.title = req.body.title;
+  if (req.body.content)post.content = req.body.content;
+  if (req.body.author)post.author = req.body.author;
+
+  res.json(post);
+});
 
 //CHALLENGE 5: DELETE a specific post by providing the post id.
+app.delete("/posts/:id", (req, res) => {
+  const postId = parseInt(req.params.id);
+  const postIndex = posts.findIndex((p) => p.id === postId);
+  if (postIndex !== -1) {
+    posts.splice(postIndex, 1);
+    res.status(200).json({ message: "Post deleted successfully" });
+  } else {
+    res.status(404).json({ message: "Post not found" });
+  }
+});
 
 app.listen(port, () => {
   console.log(`API is running at http://localhost:${port}`);
